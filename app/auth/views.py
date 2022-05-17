cb28ac56ce587508a95c1e21a5475dae0cd2d76b
lcmongwe <lcmongwe@gmail.com>
2022-05-17 14:16:41 +0300

    register.html created

--- app/auth/views.py
+++ app/auth/views.py
@@ -28,7 +28,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "Pitch Login"
+    title = "Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 
---
+++ app/start.sh
@@ -0,0 +1,2 @@
+export SECRET_KEY=abc
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://lucy:4444@localhost/socialnet'
