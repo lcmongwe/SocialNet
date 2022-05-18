cff0d2919d7c38e7884e45abeee157eb58086e82
leonkae <leon29kae@gmail.com>
2022-05-18 16:01:47 +0300

    updated forms file

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,9 @@
 '''all application forms'''
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField,SubmitField
+
+#updates bio in user bio
+class UpdateAccount(FlaskForm):
+    bio = TextAreaField('bio')
+    submit = SubmitField('update bio')
+    
