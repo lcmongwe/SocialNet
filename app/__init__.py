7ecd0c7258aa98573d7ff6c9dd5395f319622bdd
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-18 20:48:51 +0300

    update app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 """aplication factory"""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 from app.config import config_options
 
 # 
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,8 +16,9 @@ def create_app(config_name):
     # APP CONFIGS
     app.config.from_object(config_options[config_name])
     
-    # flask imports
+    # flask ext initializations.
     db.init_app(app)
+    login_manager.init_app(app)
     
     # blueprints
     from .main.urls import main, auth
