8d8ab07bae4d882cab84b2be881fbac63510484b
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-20 00:34:37 +0300

    ft: create upload form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,1 +1,10 @@
 '''all application forms'''
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
+from wtforms import ValidationError
+
+
+class UploadPost(FlaskForm):
+    body=TextAreaField('Text', validators=[DataRequired(), Length(min=1, max=140)])
+    submit=SubmitField('Submit')
