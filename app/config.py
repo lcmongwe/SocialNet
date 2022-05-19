d0fe7c46d2d81e9118fdf245a79096f817c8bd5c
Willbeckh <wileyhndirangu04@gmail.com>
2022-05-20 01:35:16 +0300

    update app config

--- app/config.py
+++ app/config.py
@@ -1,16 +1,16 @@
 import os
 
 
-
 class Config:
     """
     Class that sets the app cofiguration variables
     """
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or '<try-guessing-one-might-work>'
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
 
 class ProdConfig(Config):
     pass
@@ -20,8 +20,6 @@ class DevConfig(Config):
     """
     Development configuration settings
     """
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://lucy:4444@localhost/socialnet'
-
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     DEBUG = True
 
